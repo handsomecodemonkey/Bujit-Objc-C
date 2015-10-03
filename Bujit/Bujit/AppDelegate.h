7ff31cc77b660749dfc6eeeb5335483961cd2c19
Erick Kusnadi <erick@handsomecodemonkey.com>
2015-10-02 22:30:47 -0700

    Added the project to the project folder

---
+++ Bujit/Bujit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bujit
+//
+//  Created by Erick Kusnadi on 10/2/15.
+//  Copyright © 2015 Handsome Code Monkey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
