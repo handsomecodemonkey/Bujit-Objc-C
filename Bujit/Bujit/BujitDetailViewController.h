31ff942abfb584881a3bfd70ddc410b751168acc
Erick Kusnadi <erick@handsomecodemonkey.com>
2015-10-09 17:43:46 -0700

    Added Bujit Table View Controller and Detail View Controller

---
+++ Bujit/Bujit/BujitDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BujitDetailViewController.h
+//  Bujit
+//
+//  Created by Erick Kusnadi on 10/9/15.
+//  Copyright © 2015 Handsome Code Monkey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BujitDetailViewController : UIViewController
+
+@end
