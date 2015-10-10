b89fb2a29347c05c85ec4cc55236bd0f5c28e6e7
Erick Kusnadi <erick@handsomecodemonkey.com>
2015-10-09 20:23:03 -0700

    Added the AddBujitViewController

---
+++ Bujit/Bujit/AddBujitViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddBujitViewController.m
+//  Bujit
+//
+//  Created by Erick Kusnadi on 10/9/15.
+//  Copyright © 2015 Handsome Code Monkey. All rights reserved.
+//
+
+#import "AddBujitViewController.h"
+
+@interface AddBujitViewController ()
+
+@end
+
+@implementation AddBujitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
