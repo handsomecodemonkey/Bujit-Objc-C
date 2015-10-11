97618c6ef8e07e2775dbc48917d0ea7d4209d152
Erick Kusnadi <erick@handsomecodemonkey.com>
2015-10-10 22:52:17 -0700

    Added scrolling to the addbujitviewcontroller

--- Bujit/Bujit/AddBujitViewController.m
+++ Bujit/Bujit/AddBujitViewController.m
@@ -15,9 +15,13 @@
 @property (weak, nonatomic) IBOutlet UITextField *budgetNameTextfield;
 @property (weak, nonatomic) IBOutlet UITextField *initialDepositTextField;
 
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
 @end
 
 @implementation AddBujitViewController
+
+#pragma mark - View lifecycle
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -25,6 +29,23 @@
     
     self.budgetNameTextfield.delegate = self;
     self.initialDepositTextField.delegate = self;
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(backgroundTap:)];
+    tapGesture.numberOfTapsRequired = 1;
+    [self.view addGestureRecognizer: tapGesture];
+
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidHide:) name:UIKeyboardDidHideNotification object:nil];
+}
+
+-(void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -56,14 +77,37 @@
     return YES;
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - UIGestureRecognizers
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)backgroundTap: (UIGestureRecognizer *) gr{
+    [self.view endEditing:YES];
 }
-*/
+
+#pragma mark - Keyboard Notifications
+
+-(void)keyboardDidShow:(NSNotification *)note{
+    NSDictionary *info = [note userInfo];
+    CGRect keyboardRect = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+    keyboardRect = [self.view convertRect:keyboardRect fromView:nil];
+    
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0, 0, keyboardRect.size.height, 0);
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.scrollView.contentInset = contentInsets;
+        self.scrollView.scrollIndicatorInsets = contentInsets;
+    }];
+    
+}
+
+-(void)keyboardDidHide:(NSNotification *)note {
+    
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0, 0, 10, 0);
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.scrollView.contentInset = contentInsets;
+        self.scrollView.scrollIndicatorInsets = contentInsets;
+    }];
+    
+}
 
 @end
